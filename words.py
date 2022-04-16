f497126d74fcdbcc99d68db64b74e754fd0c0e60
seju12345678 <102609680+seju12345678@users.noreply.github.com>
2022-04-16 13:56:55 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+def load_words():
+    WORDLIST_FILENAME="words.txt"
+    inFile = open(WORDLIST_FILENAME,"r")
+    line=inFile.readline()
+    word_list = line.split()
+    return word_list
+
+
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word 
+ 
